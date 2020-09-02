5b8c4fd45936984fdddb9115be4fc7431aadfadd
ProfGrudzien <70582964+ProfGrudzien@users.noreply.github.com>
2020-09-02 15:24:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,59 +1,3 @@
-const questions=[
-  {
-    categorie: "Entrées, Sorties et Variables",
-    question: "Comment affecter une valeur à une variable ?",
-    reponse: "On utilise le symbole = entre le nom de la variable et sa valeur.",
-    exemple: "a = 12"
-  },
-  {
-    categorie: "Entrées, Sorties et Variables",
-    question: "Comment afficher la valeur d'une variable ?",
-    reponse: "On utilise le mot-clé <em>print</em>.",
-    exemple : "print(a)",
-  },
-  {
-    categorie: "Entrées, Sorties et Variables",
-    question: "Comment demander une valeur à l'utilisateur&nbsp;?",
-    reponse: "On utilise le mot-clé <em>input</em>.",
-    exemple: 'nom = input("Votre nom : ")',
-  },
-  {
-    categorie: "Instructions conditionnelles",
-    question: "Comment écrire un test <em>si</em> ?",
-    reponse: "On utilise le mot-clé <em>if</em>.",
-    exemple: 'if note >= 10 :<br/>&nbsp;&nbsp;print("bravo !")',
-  },
-  {
-    categorie: "Instructions conditionnelles",
-    question: "Comment écrire un test <em>si ... sinon</em> ?",
-    reponse: "On utilise les mot-clés <em>if</em> et <em>else</em>.",
-    exemple: 'if note >= 10 :<br/>&nbsp;&nbsp;print("bravo !")<br/>else :<br/>&nbsp;&nbsp;print("dommage...")',
-  },
-  {
-    categorie: "Instructions conditionnelles",
-    question: "Comment écrire un test <em>si ... sinon si ... sinon</em> ?",
-    reponse: "On utilise les mot-clés <em>if</em>, <em>elif</em> et <em>else</em>.",
-    exemple: 'if note >= 15 :<br/>&nbsp;&nbsp;print("excellent !")<br/>elif note >= 10 :<br/>&nbsp;&nbsp;print("bravo !")<br/>else :<br/>&nbsp;&nbsp;print("dommage...")',
-  },
-  {
-    categorie: "Boucles",
-    question: "Comment répéter 10 fois une instruction ?",
-    reponse: "On utilise une boucle <em>pour</em> et le mot-clé <em>range</em>.",
-    exemple: 'for i in range(10) :<br/>&nbsp;&nbsp;print("coucou")',
-  },
-]
-
-const couleurs = {
-  "Entrées, Sorties et Variables" : "vert",
-  "Instructions conditionnelles" : "rouge",
-  Boucles : "bleu",
-  "Chaines de caractères" : "blanc",
-  "Listes et tuples" : "blanc",
-  "Dictionnaires" : "blanc",
-  "Fonctions" : "blanc",
-  "Fichiers" : "blanc",
-}
-
 function createCard(question) {
   const card = document.createElement("div")
   card.className = "flip-card"
@@ -71,5 +15,25 @@ function createCard(question) {
   return card
 }
 
-const body = document.querySelector("body")
-questions.forEach(question => body.appendChild(createCard(question)))
+function showCards(categorie) {
+    const container = document.querySelector(".container")
+    container.innerHTML = ""
+    if (categorie == "Toutes les cartes") {
+        questions.forEach(question => container.appendChild(createCard(question)))
+    }
+    else {
+        questions.filter(question => question.categorie == categorie)
+                 .forEach(question => container.appendChild(createCard(question)))
+    }
+}
+
+function handleClick(event) {
+    Array.from(document.querySelectorAll(".menu"))
+         .forEach(div => div.className = "menu")
+    event.target.className = "menu selected"
+    showCards(event.target.textContent)
+}
+
+showCards("Toutes les cartes")
+document.getElementById("toutes").className = "menu selected"
+document.querySelector("body").addEventListener("click", handleClick)
