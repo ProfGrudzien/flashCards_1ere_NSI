728b823fde96d32af0b144e9c145f9b08e91987a
ProfGrudzien <70582964+ProfGrudzien@users.noreply.github.com>
2020-09-14 08:22:15 +0200

    Update cards.js

--- cards.js
+++ cards.js
@@ -3,7 +3,7 @@ const questions=[
     categorie: "Entrées, Sorties et Variables",
     question: "Comment affecter une valeur à une variable ?",
     reponse: "On utilise le symbole = entre le nom de la variable et sa valeur.",
-    exemple: "a = 12"
+    exemple: 'n = 12<br/>prenom = "Anna"'
   },
   {
     categorie: "Entrées, Sorties et Variables",
@@ -21,19 +21,43 @@ const questions=[
     categorie: "Entrées, Sorties et Variables",
     question: "Comment afficher un nombre et un texte ?",
     reponse: "On utilise le mot-clé <em>print</em>.",
-    exemple : "print(12, pomme)",
-  },
-  {
-    categorie: "Entrées, Sorties et Variables",
-    question: "Comment demander une valeur à l'utilisateur&nbsp;?",
-    reponse: "On utilise le mot-clé <em>input</em>.",
-    exemple: 'nom = input("Votre nom : ")',
+    exemple : 'print(12, "pommes")',
   },
   {
     categorie: "Entrées, Sorties et Variables",
     question: "Comment convertir un texte en nombre entier&nbsp;?",
     reponse: "On utilise le mot-clé <em>int</em>.",
-    exemple: 'a = int("1.2")',
+    exemple: 'a = int("12")',
+  },
+  {
+    categorie: "Entrées, Sorties et Variables",
+    question: 'Comment convertir un texte en nombre "décimal"&nbsp;?',
+    reponse: "On utilise le mot-clé <em>float</em>.",
+    exemple: 'a = float("1.2")',
+  },
+  {
+    categorie: "Entrées, Sorties et Variables",
+    question: 'Comment convertir un nombre en un texte&nbsp;?',
+    reponse: "On utilise le mot-clé <em>str</em>.",
+    exemple: 'a = str(125)<br/>b = str(1.25)',
+  },
+  {
+    categorie: "Entrées, Sorties et Variables",
+    question: "Comment demander un texte à l'utilisateur&nbsp;?",
+    reponse: "On utilise le mot-clé <em>input</em>.",
+    exemple: 'nom = input("Votre nom : ")',
+  },
+  {
+    categorie: "Entrées, Sorties et Variables",
+    question: "Comment demander un nombre à l'utilisateur&nbsp;?",
+    reponse: "On utilise un des mots-clés <em>int</em> ou <em>float</em> couplé avec <em>input</em>.",
+    exemple: 'n = int(input("Votre nombre entier : "))<br/>d = float(input("Votre nombre : "))',
+  },
+  {
+    categorie: "Entrées, Sorties et Variables",
+    question: "Comment concaténer deux textes&nbsp;?",
+    reponse: "On utilise l'opérateur <em>+</em> entre les deux textes.",
+    exemple: 'prenom = "Anna"<br/>txt = "Bonjour "+prenom+" !"',
   },
   {
     categorie: "Instructions conditionnelles",
